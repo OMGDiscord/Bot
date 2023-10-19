f902891a17736887389f9986963ee7b23d3ce391
SteavenGamerYT <omarhanykasban706@gmail.com>
2023-10-19 18:22:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS]
 });
 
-const GENERAL_CHANNEL_ID = '1155119794102747277';
+const GENERAL_CHANNEL_ID = '1164581616845328585';
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
